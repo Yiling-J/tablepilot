d7d422c9c318c851f22c9e9857680e7950d1af21
Yiling-J <njjyl723@gmail.com>
2025-03-31 22:35:01 +0800

    add some snapshot tests

---
+++ tests/cli/snapshot_test.go
@@ -0,0 +1,125 @@
+package cli
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/Yiling-J/tablepilot/cmd/cli"
+	"github.com/jarcoal/httpmock"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
+)
+
+func compareCSVFiles(t *testing.T, file1, file2 string) {
+	f1, err := os.Open(file1)
+	require.NoError(t, err)
+	defer f1.Close()
+
+	f2, err := os.Open(file2)
+	require.NoError(t, err)
+	defer f2.Close()
+
+	r1 := csv.NewReader(f1)
+	r2 := csv.NewReader(f2)
+
+	for {
+		row1, err1 := r1.Read()
+		row2, err2 := r2.Read()
+
+		if err1 != nil || err2 != nil {
+			require.Equal(t, err1, err2)
+			return
+		}
+		require.Equal(t, row1, row2)
+	}
+}
+
+type snapshot struct {
+	Request  string `json:"request"`
+	Response string `json:"response"`
+}
+
+func TestIntegrationCLI_Snapshots(t *testing.T) {
+	tests := []struct {
+		snapshot string
+		example  string
+		prepare  [][]string
+	}{
+		{"recipes_simple", "recipes_simple/recipes.json", [][]string{}},
+		{"recipes_ai_columns", "recipes_ai_columns/recipes.json", [][]string{}},
+		{"recipes_cmi", "recipes_cuisine_meal_ingredients/recipes.json", [][]string{}},
+		{"recipes_by_country", "recipes_by_country/recipes.json", [][]string{}},
+		{"recipes_for_customers", "recipes_for_customers/recipes.json", [][]string{
+			{"create", "../../examples/recipes_for_customers/customers.json"},
+			{"generate", "customers", "-c", "5", "-b", "5"},
+		}},
+		{"imdb_movie_haiku", "imdb_movie_haiku/haiku.json", [][]string{}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.snapshot, func(t *testing.T) {
+			t.Setenv("TABLEPILOT_SNAPSHOT_TEST", tt.snapshot)
+			defer func() { _ = os.Remove("test.db") }()
+			defer func() { _ = os.Remove("tmp.csv") }()
+
+			httpmock.Activate()
+			defer httpmock.DeactivateAndReset()
+
+			counter := 0
+			snapshots := []snapshot{}
+			raw, err := os.ReadFile("../snapshots/" + tt.snapshot + ".json")
+			require.NoError(t, err)
+			json.Unmarshal(raw, &snapshots)
+
+			httpmock.RegisterResponder(
+				"POST", "https://models.inference.ai.azure.com/chat/completions",
+				func(r *http.Request) (*http.Response, error) {
+					b, err := io.ReadAll(r.Body)
+					require.NoError(t, err)
+					require.Equal(t, string(b), snapshots[counter].Request)
+					resp := httpmock.NewStringResponse(200, snapshots[counter].Response)
+					resp.Header.Add("content-type", "application/json")
+					counter++
+					return resp, nil
+				},
+			)
+
+			root := &cobra.Command{
+				Use:   "",
+				Short: "",
+				CompletionOptions: cobra.CompletionOptions{
+					HiddenDefaultCmd: true,
+				},
+			}
+			_ = cli.BuildCLI(root)
+
+			for _, command := range tt.prepare {
+				command = append(command, "--config", "test.toml")
+				root.SetArgs(command)
+				err = root.Execute()
+				require.NoError(t, err)
+			}
+
+			p := "../../examples/" + tt.example
+			b, err := os.ReadFile(p)
+			require.NoError(t, err)
+			tableName := gjson.GetBytes(b, "name").String()
+			root.SetArgs([]string{
+				"create", p, "--config", "test.toml",
+			})
+			err = root.Execute()
+			require.NoError(t, err)
+
+			root.SetArgs([]string{"generate", tableName, "-c", "5", "-b", "3", "--config", "test.toml", "--saveto", "tmp.csv"})
+			err = root.Execute()
+			require.NoError(t, err)
+
+			compareCSVFiles(t, "tmp.csv", "../snapshots/"+tt.snapshot+".csv")
+		})
+	}
+}
