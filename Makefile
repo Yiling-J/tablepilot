0a7d48dea1b48dbaf4d81810b9572b8fbeb92d4a
Yiling-J <njjyl723@gmail.com>
2025-05-02 14:00:48 +0800

    embed server config file

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: test lint gen run build build-ui install snapshots
+.PHONY: test lint gen run build build-ui install snapshots tauri-dev
 
 test:
 	go test ./...
@@ -16,3 +16,6 @@ install:
 	go install
 snapshots:
 	cd tests/cli && go run snapshot.go
+tauri-dev:
+	go build -o "build/tablepilot-$(shell go run host/host.go)"
+	cd ui && pnpm tauri dev
--- ui/src-tauri/src/lib.rs
+++ ui/src-tauri/src/lib.rs
@@ -1,33 +1,69 @@
-use tauri_plugin_shell::ShellExt;
+use std::sync::{Arc, Mutex};
+
+use tauri::Manager;
 use tauri_plugin_shell::process::CommandEvent;
+use tauri_plugin_shell::ShellExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::LogDir {
+                        file_name: Some("logs".to_string()),
+                    },
+                ))
+                .build(),
+        )
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
+            let config_file_path = app
+                .path()
+                .resolve("config.toml", tauri::path::BaseDirectory::Resource)
+                .unwrap();
             let sidecar_command = app.shell().sidecar("tablepilot").unwrap();
-            let (mut rx, mut _child) = sidecar_command
+            let (mut rx, child) = sidecar_command
                 .args(["serve"])
+                .current_dir(config_file_path.parent().unwrap())
                 .spawn()
-                .expect("Failed to start tablepilot server");
+                .unwrap();
 
-	    tauri::async_runtime::spawn(async move {
-		// read events such as stdout
-		while let Some(event) = rx.recv().await {
-		    if let CommandEvent::Stderr(line_bytes) = event {
-			let line = String::from_utf8_lossy(&line_bytes);
-			println!("Tablepilot server: {}", line);
-		    }
-		}
-	    });
-            if cfg!(debug_assertions) {
-                app.handle().plugin(
-                    tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
-                        .build(),
-                )?;
-            }
+            tauri::async_runtime::spawn(async move {
+                // read events such as stdout
+                while let Some(event) = rx.recv().await {
+                    match event {
+                        CommandEvent::Stdout(line_bytes) => {
+                            let line = String::from_utf8_lossy(&line_bytes);
+                            log::info!("Tablepilot server (stdout): {}", line);
+                        }
+                        CommandEvent::Stderr(line_bytes) => {
+                            let line = String::from_utf8_lossy(&line_bytes);
+                            log::error!("Tablepilot server (stderr): {}", line);
+                        }
+                        _ => {} // Handle any other events if needed
+                    }
+                }
+            });
+
+            let window = app.get_webview_window("main").unwrap();
+            let arc_child = Arc::new(Mutex::new(Some(child)));
+            let child_clone = Arc::clone(&arc_child);
+
+            window.on_window_event(move |event| {
+                if let tauri::WindowEvent::CloseRequested { .. } = event {
+                    let mut child_lock = child_clone.lock().unwrap();
+                    if let Some(mut child_process) = child_lock.take() {
+                        if let Err(e) = child_process.write("Exit message from Rust\n".as_bytes()) {
+                            println!("Fail to send to stdin of Tablepilot: {}", e);
+                        }
+
+                        if let Err(e) = child_process.kill() {
+                            eprintln!("Failed to kill child process: {}", e);
+                        }
+                    }
+                }
+            });
             Ok(())
         })
         .run(tauri::generate_context!())
